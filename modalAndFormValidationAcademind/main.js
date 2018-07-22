f77a65c80fe81f22142963b40c70abd327d41b53
milan.zivanov <milan.zivanov@gmail.com>
2018-07-22 12:51:05 +0200

    modal and form validation simple academind

---
+++ modalAndFormValidationAcademind/main.js
@@ -0,0 +1,129 @@
+// FIXME: good example
+// TODO: 
+const backdrop = document.querySelector(".backdrop");
+const modal = document.querySelector(".modal");
+const imageSf = document.querySelectorAll(".trip-image");
+
+function modalOpen() {
+    backdrop.style.display = 'block';
+    modal.style.display = 'block';
+}
+
+function modalClose() {
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+}
+
+// for(let i = 0; i < imageSf.length; i++) {
+//     imageSf[i].addEventListener('click', modalOpen);
+// }
+
+// for of
+for(let i of imageSf) {
+    console.log(i);
+    i.addEventListener('click', modalOpen);
+}
+
+backdrop.addEventListener('click', modalClose);
+
+
+// slide menu
+
+var hamburgerButton = document.querySelector('.hamburger__button');
+var mobileNav = document.querySelector('.mobile');
+
+function openMobile() {
+    mobileNav.classList.add('open');
+}
+
+function closeMobile() {
+    mobileNav.classList.remove('open');
+}
+
+hamburgerButton.addEventListener('click', openMobile);
+mobileNav.addEventListener('click', closeMobile);
+
+
+
+// learning
+
+// import fetch from 'node-fetch';
+// or
+// const fetch = require('node-fetch');
+
+// async function fetchAvatarUrl(userId) {
+    
+//     // async await
+//     // const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+//     const response = await fetch(`http://localhost:3000/comments/${userId}`);
+
+//     const data = await response.json();
+//     console.log(data.body);
+    
+//     // response
+//     data
+//     return data.body
+
+//     // With Promise
+//     // return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+//     //     .then(response => response.json())
+//     //     .then(data => data.username);
+// }
+
+// const result = fetchAvatarUrl(1);
+// result
+
+// // map
+// var animals = [
+//     {name: 'Misko', vrsta: 'pas'},
+//     {name: 'Mia', vrsta: 'macka'},
+//     {name: 'Bucko', vrsta: 'pas'},
+//     {name: 'Linda', vrsta: 'konj'}
+// ];
+
+// // var names = animals.map(function(animal) {
+// //     return animal.name;
+// // });
+// var names = animals.map((animal) => animal.vrsta)
+// console.log(names);
+
+// var names = [];
+// for(let i = 0; i < animals.length; i++) {
+//     names.push(animals[i].name);
+// }
+// console.log(names);
+
+
+// // TODO: promises by wes bos
+
+// function sleep(ms) {
+//     return new Promise((resolve, reject) => {
+//         if(ms <= 300) {
+
+//         }
+//         setTimeout(() => resolve(`Slept for ${ms}`), ms)
+//     });
+// }
+
+// sleep(500)
+//     .then((result) => {
+//         console.log(result);
+//         return sleep(1000);
+//     })
+//     .then((result) => {
+//         console.log(result);
+//         return sleep(1500);
+//     })
+//     .then((result) => {
+//         console.log(result);
+//         return sleep('i slept enough!!!');
+//     })
+
+//     // async await
+
+//     async function go() {
+//         await sleep(1000);
+//         const responese = await sleep(800);
+//         console.log(response);
+//     }
+
