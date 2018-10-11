0b460e829ad82880f85f42b9d442ece5e13a1697
milan.zivanov <milan.zivanov@gmail.com>
2018-10-11 23:50:12 +0200

    c to f

--- weather-ap-es6/weather-data.js
+++ weather-ap-es6/weather-data.js
@@ -12,7 +12,8 @@ export const WEATHER_PROXY_HANDLER = {
     },
     set: function(target, property, value) {
         // C to F
-        const newValue = (value * 1.8 + 32).toFixed(2) + 'F.';
+        // const newValue = (value * 1.8 + 32).toFixed(2) + 'F.';
+        const newValue = value + 'C';
         return Reflect.set(target, property, newValue);
     }
 };
